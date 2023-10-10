26a2d0caf22122442b0579f482b0620a16562577
Shapka21 <128103515+Shapka21@users.noreply.github.com>
2023-10-10 13:14:08 +0300

    Add files via upload

---
+++ configure/env.py
@@ -0,0 +1,7 @@
+config = {
+    'name': 'MireaScheduleBot',
+    "token": "",  # Bot token
+    "sheet_id": '1XyDOXPbL1ZsISUnwrxSI9iXO4p5S67DXAok2H8TdauA',
+    "google_api_json": 'configure/example.json',  # google api json path
+    "url": ""  # Ngrok URL
+}
